816f622653323b8065e04e45bca4ce3ba637eec1
kayakingCellist <boyds@southwestern.edu>
2020-07-27 20:52:49 -0500

    added card and loading modal example

--- {{cookiecutter.project_slug}}/source/web/{{cookiecutter.project_slug}}-web.cc
+++ {{cookiecutter.project_slug}}/source/web/{{cookiecutter.project_slug}}-web.cc
@@ -5,6 +5,8 @@
 #include <iostream>
 
 #include "web/web.h"
+#include "prefab/Card.h"
+#include "prefab/LoadingModal.h"
 
 #include "../example.h"
 
@@ -15,6 +17,13 @@ UI::Document doc("emp_base");
 int main()
 {
   doc << "<h1>Hello, browser!</h1>";
+  // Prefab Card
+  emp::prefab::Card first_card("INIT_OPEN");
+  doc << first_card;
+  first_card.AddHeaderContent("Welcome to your first prefab card!");
+  first_card.AddBodyContent("What will you create next?");
   std::cout << "Hello, console!" << std::endl;
+  
+  emp::prefab::CloseLoadingModal();
   return example();
 }
