8783fdf754b995e973a2a2eca54bba4951746741
Katherine Perry <perryk12@msu.edu>
2020-08-27 09:45:32 -0400

    Add post_gen_project script to initialize emsdk and submodules

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Automate git repository and submodule initialization
+# Adopted from https://github.com/devosoft/cookiecutter-empirical-project#quickstart
+git init
+git add .
+git submodule add https://github.com/philsquared/Catch.git third-party/Catch
+git submodule add -b master https://github.com/devosoft/Empirical.git third-party/Empirical
+git submodule init
+git submodule update --recursive --init
+
+# Install the Emscripten Compiler
+git submodule add https://github.com/emscripten-core/emsdk.git third-party/emsdk
+cd third-party/emsdk/
+./emsdk install 1.38.48
+./emsdk activate 1.38.48
+
+
+#git commit -m "Initial commit"
+
+#git remote add origin https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git
+#git push origin master || echo 'speculative initial push failed, try setting up repo on github first.'
