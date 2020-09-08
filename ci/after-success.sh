bf0b58903e72f6e215a4a0749c17dcdbd21a5830
perryk12 <55548791+perryk12@users.noreply.github.com>
2020-09-08 09:40:44 -0400

    include git config in after-success

--- ci/after-success.sh
+++ ci/after-success.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # After success travis code
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 cd ../cut-cookie
 printf "\n\nenv:\n  global:\n    secure:%suTo92v1HA9mhToHw8b9BeaJn3SQhLCuMZDbuzA7DvuP2mEsPx9T2SzRWLN38hpKlZ7Yf0de5/o8o+tGMf2917XUeSuba4y97H7lB7SmpvBAukYff/0aTTdEBXIfgM+4XOFWCpsgKP+N+91WeSXb4zccx8VpzNfb6H9FJ3R2P0TFQkuz6CLcy2IHZCRw9r/+61/CuIpaek9PyAoyv0gzlmoMbbBvHvUoKLI/uLgpxd7qTLZOhFv0NvxtPXWZnhAhLGNSNUQ5hadiHfor86WNCOJNuB0y4APSGGWZ0SRZy28xmw+eO2WLWUR4E8pLicwlFCTZIfYXpy1LyMWRh0ZcLQjei9VevCsHuSQ0Kgyb6BV2MOeUbcCGy/xX6XwnmWOJtdK0uTkgFJjrFnsj/VhEauxtfT2P9VbwxAAi3IK/mz/dtY7f0jymGNUoALQlm6hwVSv4o/bBJXsBZUNX6+ErLmqWmsfjUco1zlVdewjsTF4wPv72P7+UfxBNjWLaawk+dU5Z1cf84FBiE4N8ohD1bGITVu1boj0bfdlAMYMu+1Q9r7hXz48MMz0/90eqWB/yDlXrxA1kLvhmMhRm3xK+Gxqeu0W609mDNDh+41OCRjmuolzQKAAGXmDwQi1RJxC/R1XsN96+00imPHz7W28cKaWYc/sLXPOPdbD2uv1wsgdA=" " " >> .travis.yml
 cat .travis.yml #for debugging
