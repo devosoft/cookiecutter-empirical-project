4739597bd4ebccd44fa9a3273513ad8d4b785b70
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-10-01 17:57:42 -0400

    Pin to Emscripten 1.38.48

--- {{cookiecutter.project_slug}}/third-party/install_emsdk.sh
+++ {{cookiecutter.project_slug}}/third-party/install_emsdk.sh
@@ -9,11 +9,11 @@ cd emsdk
 # Fetch the latest version of the emsdk (not needed the first time you clone)
 git pull
 
-# Download and install the latest SDK tools.
-./emsdk install latest
+# Download and install pinned SDK tools.
+./emsdk install 1.38.48
 
-# Make the "latest" SDK "active" for the current user. (writes ~/.emscripten file)
-./emsdk activate latest
+# Make the pinned SDK "active" for the current user. (writes ~/.emscripten file)
+./emsdk activate 1.38.48
 
 # Activate PATH and other environment variables in the current terminal
 source ./emsdk_env.sh
