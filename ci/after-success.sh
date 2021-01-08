51761629536ac640bc0544a9bb09c76f0faa3a30
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-08 18:05:07 -0500

    Use GITHUB_ACTOR, GITHUB_REPOSITORY env vars

--- ci/after-success.sh
+++ ci/after-success.sh
@@ -5,7 +5,7 @@ set -x
 # After success ci code
 cd ../cut-cookie/cookiecutter-empirical-project
 make clean
-git remote add tokenorigin "https://${GH_TOKEN}@github.com/devosoft/cookiecutter-empirical-project.git" > /dev/null 2>&1
+git remote add tokenorigin "https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" > /dev/null 2>&1
 git checkout -b cut-cookie
 git add .
 git commit --message "GitHub Action ${GITHUB_RUN_NUMBER}"
