1abd17419663939f27c5119d9405d130e72f39b4
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-08 15:57:39 -0500

    Try fixing up after success push

--- ci/after-success.sh
+++ ci/after-success.sh
@@ -5,11 +5,11 @@ set -x
 # After success ci code
 cd ../cut-cookie/cookiecutter-empirical-project
 make clean
-git remote add origin "https://mmore500:${GH_TOKEN}@github.com/devosoft/cookiecutter-empirical-project.git" > /dev/null 2>&1
+git remote add tokenorigin "https://${GH_TOKEN}@github.com/devosoft/cookiecutter-empirical-project.git" > /dev/null 2>&1
 git checkout -b cut-cookie
 git add .
 git commit --message "GitHub Action ${GITHUB_RUN_NUMBER}"
 # fallback adapted from https://stackoverflow.com/a/29380765
-git push --force --quiet origin cut-cookie || \
+git push --force --quiet tokenorigin cut-cookie || \
   ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts; \
-  git push --force --quiet origin cut-cookie
+  git push --force --quiet tokenorigin cut-cookie
