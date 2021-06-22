32e0c3f96ba41bc31f9de855313b3fa107828d58
Tait Weicht <taitweicht@icloud.com>
2021-06-22 11:30:51 -0700

    Fix definition

--- {{cookiecutter.project_slug}}/include/{{cookiecutter.project_slug}}/example.hpp
+++ {{cookiecutter.project_slug}}/include/{{cookiecutter.project_slug}}/example.hpp
@@ -12,16 +12,16 @@
 void SetupProjectConfig({{ cookiecutter.project_name.replace(' ', '') }}Config & config)  {
   auto specs = emp::ArgManager::make_builtin_specs(&config);
   emp::ArgManager am(emp::web::GetUrlParams(), specs);
-  AddExistingConfig(am);
+  AddExistingConfig(config, am);
 }
 
 void SetupProjectConfig({{ cookiecutter.project_name.replace(' ', '') }}Config & config, int argc, char* argv[]) {
   auto specs = emp::ArgManager::make_builtin_specs(&config);
   emp::ArgManager am(argc, argv, specs);
-  AddExistingConfig(am);
+  AddExistingConfig(config, am);
 }
 
-void AddExistingConfig(emp::ArgManager & am) {
+void AddExistingConfig({{ cookiecutter.project_name.replace(' ', '') }}Config & config, emp::ArgManager & am) {
   if(std::filesystem::exists("{{cookiecutter.project_slug}}.cfg")) {
     std::cout << "Configuration read from {{cookiecutter.project_slug}}.cfg" << "\n";
     config.Read("{{cookiecutter.project_slug}}.cfg");
