9c73c5ce8f458ba34a99a3f75001d5fd4ebaad95
Tait Weicht <taitweicht@icloud.com>
2021-06-28 15:45:26 -0700

    Fix bad capture

--- {{cookiecutter.project_slug}}/source/web.cpp
+++ {{cookiecutter.project_slug}}/source/web.cpp
@@ -33,16 +33,17 @@ int main()
 
   // An example to show how the Config Panel could be used
   // to control the color of text in an HTML text area
-  UI::TextArea example_area;
+  UI::TextArea example_area("example_area");
   example_area.SetSize(cfg.SIZE(), cfg.SIZE());
-  example_config_panel.SetOnChangeFun([&](const std::string & val){
+  example_config_panel.SetOnChangeFun([](const std::string & val){
+    UI::TextArea& example_area = doc.TextArea("example_area");
     example_area.SetCSS("color", cfg.COLOR());
     example_area.Redraw();
   });
   
   doc << example_area;
 
-  std::cout << "Hello, console!" << std::endl;
+  std::cout << "Hello, console!" << "\n";
 
   return example();
 }
