86dfc94f58712efbb0474e02ad857d8869cd07e2
Tait Weicht <taitweicht@icloud.com>
2021-07-21 10:22:04 -0700

    Update to match config overhaul

--- {{cookiecutter.project_slug}}/include/{{cookiecutter.project_slug}}/ExampleConfig.hpp
+++ {{cookiecutter.project_slug}}/include/{{cookiecutter.project_slug}}/ExampleConfig.hpp
@@ -6,11 +6,12 @@ namespace {{ cookiecutter.project_slug.replace('-','_') }} {
 
   EMP_BUILD_CONFIG(Config,
     GROUP(GLOBAL_SETTINGS, "Global settings"),
+    VALUE(SEED, int, -1, "Seed for a simulation"),
     VALUE(SIZE, int, 300, "Size of example text area"),
     VALUE(COLOR, std::string, "red", "Text color for example text area"),
-    GROUP(OTHER_PARAMETERS, "Miscellaneous settings"),
+    GROUP(OTHER_SETTINGS, "Miscellaneous settings"),
     VALUE(LUNCH_ORDER, std::string, "ham on five", "What's for lunch today"),
     VALUE(HOLD_MAYO, bool, true, "Whether or not to hold the mayo"),
+    VALUE(SUPER_SECRET, bool, true, "It's a hidden switch"),
   );
-
 }
