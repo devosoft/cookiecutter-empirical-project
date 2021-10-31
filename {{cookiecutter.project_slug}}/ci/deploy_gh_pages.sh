ecc6f3b28efee86fac365e84ff984d11e9634140
Matthew Andres Moreno <mmore500.login@gmail.com>
2021-10-31 19:12:41 -0400

    Fix whitespace causing bash bad subtitution

--- {{cookiecutter.project_slug}}/ci/deploy_gh_pages.sh
+++ {{cookiecutter.project_slug}}/ci/deploy_gh_pages.sh
@@ -6,7 +6,7 @@ git config --global user.name 'Github Action'
 make badges
 cd web
 git init
-git remote add origin "https://${GITHUB_ACTOR}:${ GH_TOKEN }@github.com/${GITHUB_REPOSITORY}.git" > /dev/null 2>&1
+git remote add origin "https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" > /dev/null 2>&1
 git checkout -b gh-pages
 git add .
 git commit --message "GitHub Action ${GITHUB_RUN_NUMBER}"
